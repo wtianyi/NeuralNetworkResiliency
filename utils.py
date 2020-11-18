5f6c507df4ce83b9fda7066a7eea901c5a5ed4cc
Tianyi Wang <wtianyi1204@gmail.com>
2020-11-18 08:29:37 -0800

    reformat

--- utils.py
+++ utils.py
@@ -38,11 +38,12 @@ def set_random_seed(seed: int, using_cuda: bool = False) -> None:
 
 def get_morse_datasets(mat_path: str) -> Tuple[Dataset, Dataset, int]:
     from scipy.io import matlab
+
     data = matlab.loadmat(mat_path)
-    x = torch.from_numpy(data['images'].T.astype('float32'))
-    y = torch.from_numpy(data['labels'].astype('int').ravel())
-    x_test = torch.from_numpy(data['t_images'].T.astype('float32'))
-    y_test = torch.from_numpy(data['t_labels'].astype('int').ravel())
+    x = torch.from_numpy(data["images"].T.astype("float32"))
+    y = torch.from_numpy(data["labels"].astype("int").ravel())
+    x_test = torch.from_numpy(data["t_images"].T.astype("float32"))
+    y_test = torch.from_numpy(data["t_labels"].astype("int").ravel())
 
     num_classes = torch.unique(y).nelement()
 
@@ -69,55 +70,69 @@ def get_datasets(dataset: str) -> Tuple[Dataset, Dataset, int]:
         num_classes (:obj:`int`):
             The number of classes in the dataset
     """
-    if dataset == 'morse':
+    if dataset == "morse":
         print("| Preparing Morse dataset...")
         sys.stdout.write("| ")
-        return get_morse_datasets('./data/Morse_trainning_set.mat')
-    transform_train_CIFAR = transforms.Compose([
-        transforms.RandomCrop(32, padding=4),
-        transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),
-        transforms.Normalize(cf.mean[dataset], cf.std[dataset]),
-    ])  # meanstd transformation
+        return get_morse_datasets("./data/Morse_trainning_set.mat")
+    transform_train_CIFAR = transforms.Compose(
+        [
+            transforms.RandomCrop(32, padding=4),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize(cf.mean[dataset], cf.std[dataset]),
+        ]
+    )  # meanstd transformation
 
-    transform_train_MNIST = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize(cf.mean[dataset], cf.std[dataset]),
-    ])  # meanstd transformation
+    transform_train_MNIST = transforms.Compose(
+        [
+            transforms.ToTensor(),
+            transforms.Normalize(cf.mean[dataset], cf.std[dataset]),
+        ]
+    )  # meanstd transformation
 
-    transform_test_CIFAR = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize(cf.mean[dataset], cf.std[dataset]),
-    ])
+    transform_test_CIFAR = transforms.Compose(
+        [
+            transforms.ToTensor(),
+            transforms.Normalize(cf.mean[dataset], cf.std[dataset]),
+        ]
+    )
 
-    transform_test_MNIST = transforms.Compose([
-        # transforms.Pad(padding=2, fill=0),
-        transforms.ToTensor(),
-        transforms.Normalize(cf.mean[dataset], cf.std[dataset]),
-    ])
-    if dataset == 'cifar10':
+    transform_test_MNIST = transforms.Compose(
+        [
+            # transforms.Pad(padding=2, fill=0),
+            transforms.ToTensor(),
+            transforms.Normalize(cf.mean[dataset], cf.std[dataset]),
+        ]
+    )
+    if dataset == "cifar10":
         print("| Preparing CIFAR-10 dataset...")
         sys.stdout.write("| ")
         trainset = torchvision.datasets.CIFAR10(
-            root='./data', train=True, download=True, transform=transform_train_CIFAR)
+            root="./data", train=True, download=True, transform=transform_train_CIFAR
+        )
         testset = torchvision.datasets.CIFAR10(
-            root='./data', train=False, download=True, transform=transform_test_CIFAR)
+            root="./data", train=False, download=True, transform=transform_test_CIFAR
+        )
         num_classes = 10
-    elif dataset == 'cifar100':
+    elif dataset == "cifar100":
         print("| Preparing CIFAR-100 dataset...")
         sys.stdout.write("| ")
         trainset = torchvision.datasets.CIFAR100(
-            root='./data', train=True, download=True, transform=transform_train_CIFAR)
+            root="./data", train=True, download=True, transform=transform_train_CIFAR
+        )
         testset = torchvision.datasets.CIFAR100(
-            root='./data', train=False, download=True, transform=transform_test_CIFAR)
+            root="./data", train=False, download=True, transform=transform_test_CIFAR
+        )
         num_classes = 100
-    elif dataset == 'mnist':
+    elif dataset == "mnist":
         print("| Preparing MNIST dataset...")
         sys.stdout.write("| ")
         trainset = torchvision.datasets.MNIST(
-            root='./data', train=True, download=True, transform=transform_train_MNIST)
+            root="./data", train=True, download=True, transform=transform_train_MNIST
+        )
         testset = torchvision.datasets.MNIST(
-            root='./data', train=False, download=True, transform=transform_test_MNIST)
+            root="./data", train=False, download=True, transform=transform_test_MNIST
+        )
         num_classes = 10
     else:
         raise ValueError(f"Unrecognized dataset ({dataset})")
@@ -127,63 +142,81 @@ def get_datasets(dataset: str) -> Tuple[Dataset, Dataset, int]:
 def get_network_name(args):
     parts = []
     parts.append(args.net_type)
-    if args.training_noise_type == 'gaussian' and args.training_noise is None:
-        parts.append('noise_std-[0.0]')
-    elif args.training_noise_type == 'uniform':
-        parts.append('noise-uniform')
+    if args.training_noise_type == "gaussian" and args.training_noise is None:
+        parts.append("noise_std-[0.0]")
+    elif args.training_noise_type == "uniform":
+        parts.append("noise-uniform")
     else:
-        parts.append('noise_std-{}'.format(args.training_noise))
+        parts.append("noise_std-{}".format(args.training_noise))
     if args.training_noise_mean is None:
-        parts.append('noise_mean-[0.0]')
+        parts.append("noise_mean-[0.0]")
     else:
-        parts.append('noise_mean-{}'.format(args.training_noise_mean))
+        parts.append("noise_mean-{}".format(args.training_noise_mean))
 
-    parts.append('{}-{}'.format(args.regularization_type, args.regularization))
-    parts.append('dropout-{}'.format(args.dropout_rate))
-    parts.append('lr-{}'.format(args.lr))
-    parts.append('epochs-{}'.format(args.num_epochs))
-    parts.append('lrdecayepoch-{}'.format(args.epochs_lr_decay))
+    parts.append("{}-{}".format(args.regularization_type, args.regularization))
+    parts.append("dropout-{}".format(args.dropout_rate))
+    parts.append("lr-{}".format(args.lr))
+    parts.append("epochs-{}".format(args.num_epochs))
+    parts.append("lrdecayepoch-{}".format(args.epochs_lr_decay))
     if args.forward_samples != 1:
-        parts.append('forward-{}'.format(args.forward_samples))
+        parts.append("forward-{}".format(args.forward_samples))
     if args.optim_type == "EntropySGD":
-        parts.append('entropySGD')
+        parts.append("entropySGD")
     if args.run_name:
         parts.append(args.run_name)
 
-    file_name = '_'.join(parts)
+    file_name = "_".join(parts)
     return file_name
 
 
 # Return network & file name
-def _get_network(net_type: str, depth: int, dropout_rate: float, dataset: str, num_classes: int,
-                 widen_factor: int = 1, training_noise_type: str = "gaussian", training_noise: float = None):
-    if net_type == 'lenet':
-        if dataset == 'mnist':
+def _get_network(
+    net_type: str,
+    depth: int,
+    dropout_rate: float,
+    dataset: str,
+    num_classes: int,
+    widen_factor: int = 1,
+    training_noise_type: str = "gaussian",
+    training_noise: float = None,
+):
+    if net_type == "lenet":
+        if dataset == "mnist":
             net = LeNet(num_classes, input_size=28, input_channel=1)
-        elif dataset == 'cifar10':
+        elif dataset == "cifar10":
             net = LeNet(num_classes, input_size=32, input_channel=3)
         else:
             raise ValueError(f"Unrecognized dataset ({dataset}) for lenet")
-    elif net_type == 'resnet':
-        if dataset == 'mnist':
-            net = ResNet(depth, num_classes, use_dropout=True,
-                         dropout_rate=dropout_rate, in_channel=1)
+    elif net_type == "resnet":
+        if dataset == "mnist":
+            net = ResNet(
+                depth,
+                num_classes,
+                use_dropout=True,
+                dropout_rate=dropout_rate,
+                in_channel=1,
+            )
         else:
-            net = ResNet(depth, num_classes, use_dropout=True,
-                         dropout_rate=dropout_rate, in_channel=3)
-    elif net_type == 'wide_resnet':
+            net = ResNet(
+                depth,
+                num_classes,
+                use_dropout=True,
+                dropout_rate=dropout_rate,
+                in_channel=3,
+            )
+    elif net_type == "wide_resnet":
         net = WideResNet(depth, widen_factor, dropout_rate, num_classes)
-    elif net_type == 'mlp':
-        if dataset == 'morse':
+    elif net_type == "mlp":
+        if dataset == "morse":
             net = MLP(20, 20, [20])
         else:
             raise ValueError(f"Unrecognized dataset ({dataset}) for mlp")
     else:
         raise ValueError(f"Unrecognized net_type ({net_type})")
 
-    if training_noise_type == 'gaussian' and training_noise is None:
+    if training_noise_type == "gaussian" and training_noise is None:
         net.apply(set_gaussian_noise)
-    elif training_noise_type == 'uniform':
+    elif training_noise_type == "uniform":
         net.apply(set_uniform_noise)
     else:
         net.apply(set_gaussian_noise)
@@ -206,9 +239,16 @@ def get_network(args, num_classes: int) -> Tuple[nn.Module, str]:
         file_name (:obj:`str`):
             A file name with the arguments composed in
     """
-    net = _get_network(net_type=args.net_type, depth=args.depth, dropout_rate=args.dropout_rate,
-                       dataset=args.dataset, num_classes=num_classes, widen_factor=args.widen_factor,
-                       training_noise_type=args.training_noise_type, training_noise=args.training_noise)
+    net = _get_network(
+        net_type=args.net_type,
+        depth=args.depth,
+        dropout_rate=args.dropout_rate,
+        dataset=args.dataset,
+        num_classes=num_classes,
+        widen_factor=args.widen_factor,
+        training_noise_type=args.training_noise_type,
+        training_noise=args.training_noise,
+    )
     file_name = get_network_name(args)
     return net, file_name
 
@@ -225,7 +265,7 @@ def create_or_clear_dir(path, force=False):
             elif os.path.isdir(file_path):
                 shutil.rmtree(file_path)
             # except Exception as e:
-                # print('Failed to delete %s. Reason: %s' % (file_path, e))
+            # print('Failed to delete %s. Reason: %s' % (file_path, e))
     else:
         if force:
             os.unlink(path)
